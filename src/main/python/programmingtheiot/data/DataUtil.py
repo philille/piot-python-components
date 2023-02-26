fd865e706b89bb64dc9068fbb5a52d93839dcd23
002183289_ZhanWang <70181019+philille@users.noreply.github.com>
2023-02-25 23:07:04 -0800

    labmodule5

--- src/main/python/programmingtheiot/data/DataUtil.py
+++ src/main/python/programmingtheiot/data/DataUtil.py
@@ -9,37 +9,108 @@
 
 from json import JSONEncoder
 
+import json
+import logging
+
+from decimal import Decimal
+from json import JSONEncoder
+
 from programmingtheiot.data.ActuatorData import ActuatorData
 from programmingtheiot.data.SensorData import SensorData
 from programmingtheiot.data.SystemPerformanceData import SystemPerformanceData
 
 class DataUtil():
-	"""
-	Shell representation of class for student implementation.
-	
-	"""
-
 	def __init__(self, encodeToUtf8 = False):
-		pass
+		self.encodeToUtf8 = encodeToUtf8
+		
+		logging.info("Created DataUtil instance.")
 	
-	def actuatorDataToJson(self, data: ActuatorData = None):
-		pass
-	
-	def sensorDataToJson(self, data: SensorData = None):
-		pass
+	def actuatorDataToJson(self, actuatorData):
+		if not actuatorData:
+			logging.debug("ActuatorData is null. Returning empty string.")
+			return ""
+		
+		jsonData = json.dumps(actuatorData, indent = 4, cls = JsonDataEncoder)
+		return jsonData
+	  
+	def sensorDataToJson(self, sensorData):
+		if not sensorData:
+			logging.debug("SensorData is null. Returning empty string.")
+			return "" 
+		jsonData = json.dumps(sensorData, indent = 4, cls = JsonDataEncoder)
+		return jsonData
 
-	def systemPerformanceDataToJson(self, data: SystemPerformanceData = None):
-		pass
+	def systemPerformanceDataToJson(self, sysPerfData):
+		if not sysPerfData:
+			logging.debug("SystemPerformanceData is null. Returning empty string.")
+			return "" 
+		jsonData = json.dumps(sysPerfData, indent = 4, cls = JsonDataEncoder)
+		return jsonData
 	
-	def jsonToActuatorData(self, jsonData: str = None):
-		pass
+	def jsonToActuatorData(self, jsonData):
+		if not jsonData:
+			logging.warning("JSON data is empty or null.")
+			return None
+		jsonData = jsonData.replace("\'", "\"").replace('False', 'false').replace('True', 'true')
+		ad = ActuatorData()
+		jsonStruct = json.loads(jsonData)
+		self._updateIotData(jsonStruct, ad)
+		return ad
 	
-	def jsonToSensorData(self, jsonData: str = None):
-		pass
+	def jsonToSensorData(self, jsonData):
+		if not jsonData:
+			logging.warning("JSON data is empty or null.")
+			return None
+		jsonData = jsonData.replace("\'", "\"").replace('False', 'false').replace('True', 'true')
+		ad = SensorData()
+		jsonStruct = json.loads(jsonData)
+		self._updateIotData(jsonStruct, ad)
+		return ad
 	
-	def jsonToSystemPerformanceData(self, jsonData: str = None):
-		pass
+	def jsonToSystemPerformanceData(self, jsonData):
+		if not jsonData:
+			logging.warning("JSON data is empty or null.")
+			return None
+		jsonData = jsonData.replace("\'", "\"").replace('False', 'false').replace('True', 'true')
+		ad = SystemPerformanceData()
+		jsonStruct = json.loads(jsonData)
+		self._updateIotData(jsonStruct, ad)
+		return ad	
 	
+	def _formatDataAndLoadDictionary(self, jsonData: str, useDecForFloat: bool = False) -> dict:
+		jsonData = jsonData.replace("\'", "\"").replace('False', 'false').replace('True', 'true')
+		
+		jsonStruct = None
+		
+		if useDecForFloat:
+			jsonStruct = json.loads(jsonData, parse_float = Decimal)
+		else:
+			jsonStruct = json.loads(jsonData)
+		
+		return jsonStruct
+		
+	def _generateJsonData(self, obj, useDecForFloat: bool = False) -> str:
+		jsonData = None
+		
+		if self.encodeToUtf8:
+			jsonData = json.dumps(obj, cls = JsonDataEncoder).encode('utf8')
+		else:
+			jsonData = json.dumps(obj, cls = JsonDataEncoder, indent = 4)
+		
+		if jsonData:
+			jsonData = jsonData.replace("\'", "\"").replace('False', 'false').replace('True', 'true')
+		
+		return jsonData
+	
+	def _updateIotData(self, jsonStruct, obj):
+		varStruct = vars(obj)
+		
+		for key in jsonStruct:
+			if key in varStruct:
+				setattr(obj, key, jsonStruct[key])
+			else:
+				logging.warn("JSON data contains key not mappable to object: %s", key)
+		
 class JsonDataEncoder(JSONEncoder):
 	"""
 	Convenience class to facilitate JSON encoding of an object that
@@ -48,4 +119,3 @@ class JsonDataEncoder(JSONEncoder):
 	"""
 	def default(self, o):
 		return o.__dict__
-	
